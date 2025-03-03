42ff3b6ab90407aeee76ee7160784d56b409b505
Dmitrii <57145783+inskyeee@users.noreply.github.com>
2025-03-03 19:32:27 +0000

    Add basic pages and views, set up sign ups

--- assistant_app/urls.py
+++ assistant_app/urls.py
@@ -5,4 +5,13 @@ app_name = 'assistant_app'
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+
+    path("login/", views.login_view, name="login"),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+    path('account/', views.account_view, name='account'),
+
+    path('calculator/', views.calculator_view, name='calculator'),
+    path('courses/', views.courses_view, name='courses'),
 ]
--- assistant_app/views.py
+++ assistant_app/views.py
@@ -1,8 +1,70 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm, CalculatorForm
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     context_dict = {"title": "Welcome to the Student Assistant App",
-                "message": "Hello, world!",}
+                    "message": "Hello, world!",}
     
     return render(request, "assistant_app/home.html", context=context_dict)
+
+@login_required
+def about(request):
+    return render(request, "assistant_app/about.html")
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('assistant_app:home')
+        else:
+            return render(request, 'assistant_app/login.html', {'error': 'Invalid credentials'})
+
+    return render(request, 'assistant_app/login.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('assistant_app:home')
+    else:
+        form = RegistrationForm()
+    return render(request, 'assistant_app/register.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('assistant_app:login')
+
+@login_required
+def account_view(request):
+    return render(request, 'assistant_app/account.html')
+
+def calculator_view(request):
+    if request.method == 'POST':
+        form = CalculatorForm(request.POST)
+        if form.is_valid():
+            assignment = form.cleaned_data['assignment']
+            grade = form.cleaned_data['grade']
+            weight = form.cleaned_data['weight']
+            result = grade * weight
+        return render(request, 'assistant_app/calculator.html', {'form': form, 'result': result})
+    else:
+        form = CalculatorForm()
+    return render(request, 'assistant_app/calculator.html', {'form': form})
+
+@login_required
+def courses_view(request):
+    courses_dict = [{"name": "ADS2 2007"},
+                    {"name": "WAD 2021"},
+                    {"name": "OOSE 2008"}]
+    
+    return render(request, 'assistant_app/courses.html', {'courses': courses_dict})
